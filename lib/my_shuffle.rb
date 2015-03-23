51e205860631f44a417b7c0347b509b129538513
Katie Hoffman <ktahoffman@gmail.com>
2015-03-23 12:48:55 -0400

    add solution

--- lib/my_shuffle.rb
+++ lib/my_shuffle.rb
@@ -1,1 +1,22 @@
-# your code goes here
+class Array
+
+  def my_shuffle
+    return self if self.length <= 1
+    return [self[1], self[0]] if self.length == 2
+    reordered = self.shuffle
+    until completely_different?(reordered)
+      reordered = self.shuffle
+    end
+    reordered
+  end
+
+  def completely_different?(shuffled)
+    shuffled.each_with_index do |element,i| 
+      if element == self[i]
+        return false 
+      end
+    end
+    true
+  end
+
+end
