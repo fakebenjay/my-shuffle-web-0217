cd59c9825e188064870bfa3bd96ecb0bf3c3c039
Katie Hoffman <ktahoffman@gmail.com>
2015-03-23 12:04:17 -0400

    pass all tests

--- lib/my_shuffle.rb
+++ lib/my_shuffle.rb
@@ -1,1 +1,16 @@
-# your code goes here
+class Array
+
+  def my_shuffle
+    return self if self.length <= 1
+    result = Array.new(self.length, "whatever")
+    self.each_with_index do |element, i|
+      index = rand(0...self.length)
+      until result[index] == "whatever" && index != i
+        index = rand(0...self.length)
+      end
+      result[index] = element
+    end
+    result
+  end
+
+end
