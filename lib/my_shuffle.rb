f67097f77a72fd29630621f0bef08663329dc499
Katie Hoffman <ktahoffman@gmail.com>
2015-03-23 12:05:19 -0400

    merge with solution to fetch new tests

--- lib/my_shuffle.rb
+++ lib/my_shuffle.rb
@@ -1,16 +1,1 @@
-class Array
-
-  def my_shuffle
-    return self if self.length <= 1
-    result = Array.new(self.length, "whatever")
-    self.each_with_index do |element, i|
-      index = rand(0...self.length)
-      until result[index] == "whatever" && index != i
-        index = rand(0...self.length)
-      end
-      result[index] = element
-    end
-    result
-  end
-
-end
+# your code goes here
